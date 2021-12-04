4e9cf6ad3e3abd0697a545f02bba1b50c06de2da
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-04 07:01:30 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,25 @@
+// string
+var nama = 'josh';
+console.log("nama:", nama)
+// number
+var umur = '25';
+console.log("umur:", umur)
+
+// boolean
+var tua = true;
+umur = umur;
+if (umur>25)
+tua = true;
+else
+tua = false;
+console.log("sudah tua:", tua);
+
+// null & undifined
+var menikah = null;
+var anak;
+console.log("menikah:",menikah)
+console.log("anak:",anak)
+
+//array
+var anak_ke = [7,3,4,5,7,8,1,9]
+console.log("anak ke:",anak_ke[6])
